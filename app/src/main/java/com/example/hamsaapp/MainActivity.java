f7a5a2e20db996475be7ca4685d480140be365d1
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-09-14 14:59:54 +0300

    design main xml to do: add image

--- app/src/main/java/com/example/hamsaapp/MainActivity.java
+++ app/src/main/java/com/example/hamsaapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hamsaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //iuiyu8i7uii
         setContentView(R.layout.activity_main);
+        Log.d("hamsa","u");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("hamsa","onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("hamsa","onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("hamsa","onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
