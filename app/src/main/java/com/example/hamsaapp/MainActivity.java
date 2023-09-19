ad31bdf6a8df12de384e2c103ce529b53dd86dcf
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-09-19 11:01:03 +0300

    design main xml to do: add image

--- app/src/main/java/com/example/hamsaapp/MainActivity.java
+++ app/src/main/java/com/example/hamsaapp/MainActivity.java
@@ -13,31 +13,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //iuiyu8i7uii
         setContentView(R.layout.activity_main);
-        Log.d("hamsa","u");
+        Log.d("hamsa","onCreate");
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
-
+  
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("hamsa","onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("hamsa","onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("hamsa","onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("hamsa","onStart");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("hamsa","onStart");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("hamsa","onStart");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
