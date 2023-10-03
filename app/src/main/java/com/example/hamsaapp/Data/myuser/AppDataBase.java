9b64834652679bb3588ba54bd93bd88dee1292ac
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-10-03 12:22:24 +0300

    design main xml to do: add image

--- app/src/main/java/com/example/hamsaapp/Data/myuser/AppDataBase.java
+++ app/src/main/java/com/example/hamsaapp/Data/myuser/AppDataBase.java
@@ -1,5 +1,34 @@
 package com.example.hamsaapp.Data.myuser;
 
-public class AppDataBase {
+import android.content.Context;
 
-}
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.hamsaapp.Data.MyTask.Mytask;
+import com.example.hamsaapp.Data.MyTask.MytaskQuery;
+import com.example.hamsaapp.Data.mySubject.Mysubject;
+import com.example.hamsaapp.Data.mySubject.MysubjectQuery;
+
+@Database(entities = {MyUser.class, Mysubject.class, Mytask.class},version = 5)
+
+public abstract class AppDataBase extends RoomDatabase {
+
+
+        private static AppDataBase db;
+        public abstract Myuserquery.MyUserQuery getMyUserQuery();
+        public abstract MysubjectQuery getMySubjectQuery();
+        public abstract MytaskQuery getMyTaskQuery();
+
+        public static AppDataBase getDB(Context context) {
+            if (db == null) {
+                db = Room.databaseBuilder(context,
+                                AppDataBase.class, "database-name")
+                        .fallbackToDestructiveMigration()
+                        .allowMainThreadQueries()
+                        .build();
+
+
+            }
+        }
